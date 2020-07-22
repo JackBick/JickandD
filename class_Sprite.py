8a8f977872a75b65ee47c911eae75773d9c21538
Jack Bickersteth <jbic3585@uni.sydney.edu.au>
2020-07-22 12:27:35 +1000

    newclass

--- class_Sprite.py
+++ class_Sprite.py
@@ -1,17 +1,7 @@
-class	Sprite:
-	def	__init__(self,x,y,img):
-  	self.x=x
-    self.y=y
-    self.img=img
-
-    def move(direction):
-    	if direction == "up":
-      	self.y -=1
-      elif direction == "down":
-      	self.y +=1
-      elif direction == "left":
-      	self.x -=1
-      elif directiion == "right":
-      	self.x +=1
-
-    def 
+class Sprite:
+	def	__init__(self,x,y,dim_height,dim_width,img):
+  	    self.x=x
+        self.y=y
+		self.dim_height=dim_height
+		self.dim_width=dim_width
+        self.img=img
---
+++ main.py
@@ -0,0 +1,6 @@
+from class_Sprite.py import Sprite
+from class_Game.py import Game
+from class_Player.py import Player
+
+_game = Game()
+_player = Player(0,0,0,0,img)
