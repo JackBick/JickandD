db50b183d2b933a06b2da6e0fa4e429fd851f4eb
Jack Bickersteth <jbic3585@uni.sydney.edu.au>
2020-07-23 13:54:18 +1000

    attempt

--- class_Game.py
+++ class_Game.py
@@ -1,16 +1,18 @@
+from class_Tile import Tile
+
 class Game:
     def __init__(self,x,y):
-        board = [[" "] * x for i in range(y)]
+        board = [[Tile()] * x for i in range(y)]
         self.board=board
 
     def board_print(self):
         for i in range(len(self.board[0])):
             for j in range(len(self.board[i])):
-                print(self.board[j][i],end ="")
+                print(self.board[j][i].output,end ="")
             print()
 
     def update_player(self,player):
-        self.board[player.x][player.y]="P"
+        self.board[player.x][player.y].output="P"
 
     def old_player(self,player):
-        self.board[player.x][player.y]=" "
+        self.board[player.x][player.y].output=" "
--- class_Tile.py
+++ class_Tile.py
@@ -2,4 +2,5 @@ class Tile():
     def __init__(self):
         self.item=None
         self.is_wall=False
+        self.has_player=False
         self.output=" "
