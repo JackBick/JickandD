b422707d0ae4d077d3399421c85e712fee6681db
Jack Bickersteth <jbic3585@uni.sydney.edu.au>
2020-07-23 12:19:04 +1000

    game_1.1

--- class_Game.py
+++ class_Game.py
@@ -1,1 +1,10 @@
 class Game:
+    def __init__(self,x,y):
+        board = [[0] * x for i in range(y)]
+        self.board=board
+
+    def board_print(self):
+        for i in range(len(self.board[0])):
+            for j in range(len(self.board[i])):
+                print(self.board[i][j],end ="")
+            print()
